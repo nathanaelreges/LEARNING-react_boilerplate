6f708cc466895b53615697f2fa2752e85806d0de
Nathanael Reges <nathanaelregesdev@gmail.com>
2018-07-26 19:29:03 -0300

    Switching from CommonJs module format to ES6's on jsx files

--- src/component.jsx
+++ src/component.jsx
@@ -1,4 +1,4 @@
-const React = require('react')
+import React from 'react'
 import PropTypes from 'prop-types'
 
 class Component extends React.Component {
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+/*
 const React = require('react')
 const ReactDOM =  require('react-dom')
+*/
 import Component from './component.jsx'
 
 
