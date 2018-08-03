325eb76058c9014c9a01acb3becbc4a922b6ce52
Nathanael Reges <nathanaelregesdev@gmail.com>
2018-08-03 11:22:25 -0300

    small update

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -12,7 +12,7 @@ module.exports = merge(common, {
    },
    plugins: [
       new webpack.DefinePlugin({
-         'process.env.NODE_ENV': JSON.stringify('production')
+         'process.env.NODE_ENV': JSON.stringify('production') //I think there is no need for that anymore
       })
    ]
 })
